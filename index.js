d3ab7b40061f8827e07bd3a1b9d70391030d9108
Vincent Langat <kimutailangat033@gmail.com>
2022-06-06 17:42:17 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(myNumber){
   // Write your code here!
+  if(myNumber == 199){
+    return "This one is on me!"
+  }
+  else if (myNumber> 2000 && myNumber < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else if (myNumber > 2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(myCity){
   // Write your code here!
+if (myCity == "NYC"){
+  return "Ok, sounds good."
+}
+else{
+  return "No go.";
+}
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(myTip){
   // Write your code here!
+   
+ switch(myTip){
+   case 'generous':
+     return "Thank you so much."
+     break;
+   case 'not as generous':
+   return "Thank you."
+   break;
+   default:
+     return "Bye."
+ 
+ }
 }
